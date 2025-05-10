583b19639f8d79723ae5cf5c2690a12165577de2
gavindu-t <tqd503@york.ac.uk>
2025-05-10 01:47:15 +0100

    creating test trainingclassifer

--- testing/pytorch60minblitz/trainingclassifer.py
+++ testing/pytorch60minblitz/trainingclassifer.py
@@ -16,12 +16,12 @@ batch_size = 4
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
+                                          shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
+                                         shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
